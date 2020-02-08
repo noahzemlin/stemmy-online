e84c210ad1e50f8c011cad09debcbef9d5cf92a0
Kevin Robb <kevin.robb@ou.edu>
2020-02-08 15:52:37 -0600

    added server backend

---
+++ server/controllers/game_logic.py
@@ -0,0 +1,32 @@
+from random import randint
+
+
+class game_logic:
+
+    # define static variables
+    result = None
+
+    @staticmethod
+    def compute_result(dice_list):
+        result = 0
+        for x in dice_list:
+            result += x
+        return result
+
+    @staticmethod
+    def generate_dice(num_dice):
+        dice_list = []
+        # add num_dice values to the list
+        for i in range(num_dice):
+            # assuming all dice are d6
+            dice_list.append(randint(1,6))
+        # compute result and store to static var
+        game_logic.result = game_logic.compute_result(dice_list)
+        return dice_list
+
+    @staticmethod
+    def check_result(user_answer):
+        if user_answer == game_logic.result:
+            return True
+        else:
+            return False
--- server/main.py
+++ server/main.py
@@ -1,5 +1,6 @@
 import eventlet
 import socketio
+from controllers.game_logic import game_logic
 
 sio = socketio.Server()
 app = socketio.WSGIApp(sio)
@@ -7,10 +8,28 @@ app = socketio.WSGIApp(sio)
 @sio.event
 def connect(sid, environ):
     print('connect ', sid)
+    # create initial set of dice and send to everyone
+    dice_list = game_logic.generate_dice(num_dice=3)
+    sio.emit(dice_list)
+
+@sio.event
+def receive_answer(sid, answer):
+    print('received ', sid)
+    # check if result is correct
+    if game_logic.check_result(answer):
+        # result is correct, so note the user and add to their score TODO
+        
+        # send new dice set out since current one was guessed
+        dice_list = game_logic.generate_dice(num_dice=3)
+        sio.emit(dice_list)
+    else:
+        sio.emit("incorrect", room=sid)
 
 @sio.event
 def health(sid, data):
     print('health ', data)
+    # send "health" back to requesters
+    sio.emit("health", room=sid)
 
 @sio.event
 def disconnect(sid):
