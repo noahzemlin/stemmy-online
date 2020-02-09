333dcecbc1505b6ae22890e30193067ae6a50375
Kevin Robb <kevin.robb@ou.edu>
2020-02-08 19:54:02 -0600

    fixed some typing and modifying tuple errors

--- server/controllers/scoring.py
+++ server/controllers/scoring.py
@@ -3,12 +3,22 @@ class scoring:
 
     # list of players. each player object is a tuple containing sid, name, and score
     players = []
+    # temp list for players who have not yet entered a name
+    no_name_players = []
 
     @staticmethod
-    def add_player(sid, name):
+    def add_player(sid):
         # add a new player who just connected
-        new_player = (sid, name, 0)
-        scoring.players.append(new_player)
+        new_player = (sid, "NO_NAME", 0)
+        scoring.no_name_players.append(new_player)
+
+    @staticmethod
+    def assign_name(sid, name):
+        for p in scoring.no_name_players:
+            if p[0] == sid:
+                scoring.players.append((p[0], name, p[2]))
+                scoring.no_name_players.remove(p)
+                break
 
     @staticmethod
     def remove_player(sid):
@@ -18,17 +28,18 @@ class scoring:
                 scoring.players.remove(p)
                 break
         # update leaderboard. sort by score
-        scoring.players = sorted(scoring.players, key = lambda x: x[2]) 
+        scoring.players = sorted(scoring.players, key = lambda x: x[2])[:-1]
 
     @staticmethod
     def update_score(sid):
-        for p in scoring.players:
+        for n, p in scoring.players:
             if p[0] == sid:
-                #give one point for the correct answer
-                p[2] += 1 
+                # add one point for the correct answer
+                # replace tuple for player with new one with new score
+                scoring.players[n] = (p[0], p[1], p[2] + 1)
                 break
         # update leaderboard. sort by score
-        scoring.players = sorted(scoring.players, key = lambda x: x[2]) 
+        scoring.players = sorted(scoring.players, key = lambda x: x[2])[:-1]
     
     @staticmethod
     def get_leaderboard():
--- server/main.py
+++ server/main.py
@@ -8,13 +8,24 @@ app = socketio.WSGIApp(sio)
 
 @sio.event
 def connect(sid, environ): 
-    # users will be asked their name before being allowed to connect
     print('connect ', sid)
-    query = environ["QUERY_STRING"]
-    name = query[query.find("name=")+5:query.find("&")]
-    scoring.add_player(sid, name)
-    # don't start game until three players
-    if len(scoring.players) >= 3:
+    # users will be asked their name before being allowed to connect
+    #query = environ["QUERY_STRING"]
+    #name = query[query.find("name=")+5:query.find("&")]
+    scoring.add_player(sid)
+
+@sio.event
+def assign_name(sid, name):
+    print('received_name ', sid)
+    # assign name to player
+    scoring.assign_name(sid, name)
+
+@sio.event
+def start(sid):
+    print('starting')
+    # don't allow start game until certain number of players
+    min_players = 1
+    if len(scoring.players) >= min_players: 
         # create initial set of dice and send to everyone
         dice_list = game_logic.generate_dice(num_dice=3)
         sio.emit('new_dice', dice_list)
@@ -22,8 +33,14 @@ def connect(sid, environ):
         sio.emit('leaderboard', scoring.get_leaderboard())
 
 @sio.event
-def receive_answer(sid, answer):
-    print('received ', sid)
+def receive_answer(sid, ans):
+    try:
+        answer = int(ans)
+    except:
+        # ignore a non-numeric answer
+        return
+
+    print('received_answer ', sid)
     # check if result is correct
     if game_logic.check_result(answer):
         # result is correct, so add to the user's score
@@ -32,6 +49,7 @@ def receive_answer(sid, answer):
         dice_list = game_logic.generate_dice(num_dice=3)
         sio.emit('new_dice', dice_list)
         # update leaderboard
+        sio.emit('leaderboard', scoring.get_leaderboard())
     else:
         sio.emit("incorrect", room=sid)
 
